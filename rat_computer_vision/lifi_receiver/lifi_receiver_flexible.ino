444d85d894736f133365e143f4b6c20a15788e2b
Roman Pogodin <pogodin@phystech.edu>
2017-09-20 19:11:06 +0100

    Speed conrol changed & minor fixes

--- rat_computer_vision/lifi_receiver/lifi_receiver_flexible.ino
+++ rat_computer_vision/lifi_receiver/lifi_receiver_flexible.ino
@@ -7,7 +7,7 @@ RF24 radio(2, 3);  // For Arduino Micro
 byte addresses[][6] = {"1Node", "2Node"};
 bool role = 0; // Local state variable that controls whether this node is sending 1 or receiving 0
 
-const int kDelay = 10;
+const int kDelay = 50;
 
 Servo myservoLeft;  // create servo object to control a servo
 Servo myservoRight;  // create servo object to control a servo
@@ -37,7 +37,6 @@ void setup() {
   delay(1000);  // Pause briefly to let everything "settle"
 
   radio.startListening();  // Start the radio listening for data
-
   
   digitalWrite(12, LOW);
   digitalWrite(10, LOW);
@@ -49,8 +48,8 @@ void loop() {
 
   radio.startListening();  // Start the radio listening for data
 //  if (radio.available()) {                // Variable for the received timestamp
-    while (!radio.available()) {
-      delay(1);
+   while (!radio.available()) {
+    delay(1);
    }
    radio.read(&received_signal, sizeof(byte));  // Get the payload
    Serial.println((received_signal));
@@ -71,44 +70,45 @@ void loop() {
     subtractor = 0;
     Serial.println("STOP");
   } else if ( received_signal == 1) {
-      Serial.println("Forward");
-      leftSpeed = 180;
-      rightSpeed = 0;
-      digitalWrite(12, LOW);
-      digitalWrite(10, HIGH);
-      go = 1;
-      subtractor = 0;
-   } else if (received_signal == 2) {
-      Serial.println("Backward");
-      leftSpeed = 0;
-      rightSpeed = 180;
-      digitalWrite(12, HIGH);
-      digitalWrite(10, HIGH);
-      go = 1;
-      subtractor = 1;
-    } else if (2 < received_signal < 6) {
-      Serial.println("Turn Left");
-      leftSpeed = 0;
-      rightSpeed = 0;
-      digitalWrite(12, LOW);
-      digitalWrite(10, LOW);
-      go = 1;
-      subtractor = 2;
-    } else if (5 < received_signal < 10) {
-      Serial.println("Turn Right");
-      rightSpeed = 180;
-      leftSpeed = 180;
-      digitalWrite(12, LOW);
-      digitalWrite(10, LOW);
-      go = 1;
-      subtractor = 5;
-    } else {
-      Serial.println("No Signal Received");
-    }
-    myservoLeft.write(leftSpeed);
-    myservoRight.write(rightSpeed);
+    Serial.println("Forward");
+    leftSpeed = 180;
+    rightSpeed = 0;
+    digitalWrite(12, LOW);
+    digitalWrite(10, HIGH);
+    go = 1;
+    subtractor = 1;
+  } else if (received_signal == 2) {
+    Serial.println("Backward");
+    leftSpeed = 0;
+    rightSpeed = 180;
+    digitalWrite(12, HIGH);
+    digitalWrite(10, HIGH);
+    go = 1;
+    subtractor = 2;
+  } else if (3 <= received_signal && received_signal < 6) {
+    Serial.println("Turn Left");
+    leftSpeed = 0;
+    rightSpeed = 0;
+    digitalWrite(12, LOW);
+    digitalWrite(10, LOW);
+    go = 1;
+    subtractor = 3;
+  } else if (6 <= received_signal && received_signal < 10) {
+    Serial.println("Turn Right");
+    leftSpeed = 180;
+    rightSpeed = 180;
+    digitalWrite(12, LOW);
+    digitalWrite(10, LOW);
+    go = 1;
+    subtractor = 6;
+  } else {
+    Serial.println("No Signal Received");
+  }
+    
+  myservoLeft.write(leftSpeed);
+  myservoRight.write(rightSpeed);
 
   Serial.println("------------New round----------");
 
-  delay(kDelay + (received_signal - subtractor) * 100);
+  delay(kDelay + (received_signal - subtractor) * 50);
 }
