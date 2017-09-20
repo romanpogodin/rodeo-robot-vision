00ef3194922e313c18baccf2d12821e50f90a725
Roman Pogodin <pogodin@phystech.edu>
2017-09-20 19:16:54 +0100

    Updated to work with flexible speed

--- rat_computer_vision/arduino-keyboard.py
+++ rat_computer_vision/arduino-keyboard.py
@@ -5,25 +5,25 @@ from time import sleep
 import serial
 from pynput import keyboard
 
-ser = serial.Serial('COM9', 9600)
+ser = serial.Serial('COM9', 115200)
 
 def on_press(key):
     try:
         # ser.write(b'{0}'.format(key)[1]) # should be more effecient
         if 'w' == '{0}'.format(key)[1]:
-            ser.write(b'w')
+            ser.write(bytes([1]))
             #sleep(0.00001)        
         elif 'a' == '{0}'.format(key)[1]:
-            ser.write(b'a')
+            ser.write(bytes([3]))
             #sleep(0.00001)
         elif 's' == '{0}'.format(key)[1]:
-            ser.write(b's')
+            ser.write(bytes([2]))
             #sleep(0.00001)
         elif 'd' == '{0}'.format(key)[1]:
-            ser.write(b'd')
+            ser.write(bytes([6]))
             #sleep(0.00001)  
         elif 'c' == '{0}'.format(key)[1]:
-            ser.write(b'c')
+            ser.write(bytes([0]))
             sleep(0.0001)
     except AttributeError:
         print('special key {0} pressed'.format(key))
