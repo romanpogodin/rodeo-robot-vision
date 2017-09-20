02516441d5efde3ef5bd5eb9ebf32fdaab53e850
Roman Pogodin <pogodin@phystech.edu>
2017-09-20 18:25:53 +0100

    Updated to the current version

--- rat_computer_vision/lifi_receiver/lifi_receiver.ino
+++ rat_computer_vision/lifi_receiver/lifi_receiver.ino
@@ -7,7 +7,7 @@ RF24 radio(2, 3);  // For Arduino Micro
 byte addresses[][6] = {"1Node", "2Node"};
 bool role = 0; // Local state variable that controls whether this node is sending 1 or receiving 0
 
-const int kDelay = 50;
+const int kDelay = 20;
 
 Servo myservoLeft;  // create servo object to control a servo
 Servo myservoRight;  // create servo object to control a servo
@@ -15,12 +15,11 @@ Servo myservoRight;  // create servo object to control a servo
 
 int leftSpeed = 0;    // variable to store the servo position
 int rightSpeed = 0;
-int go = 1;
-
+int go = 0;
 
 void setup() {
-//  myservoLeft.attach(9);  // attaches the servo on pin 9 to the servo object
-//  myservoRight.attach(8);  // attaches the servo on pin 8 to the servo object
+  myservoLeft.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservoRight.attach(8);  // attaches the servo on pin 8 to the servo object
   pinMode(12, OUTPUT); // lightsource dectector
   pinMode(10, OUTPUT); // wall detector
   Serial.begin(115200);
@@ -29,56 +28,54 @@ void setup() {
   radio.setPALevel(RF24_PA_LOW); // Set the Power Level low
   radio.setDataRate(RF24_250KBPS); // Set radio rate and channel
   radio.setChannel(100);  // Set the channel (Frequency): 108 = 2.508 GHz (above WiFi)
-
+  radio.setPayloadSize(8);
   // Open a writing and reading pipe on each radio, with opposite addresses
   radio.openWritingPipe(addresses[0]);
   radio.openReadingPipe(1, addresses[1]);
 
-  delay(500);  // Pause briefly to let everything "settle"
+  delay(1000);  // Pause briefly to let everything "settle"
 
   radio.startListening();  // Start the radio listening for data
 
+  
   digitalWrite(12, LOW);
   digitalWrite(10, LOW);
 }
 
 void loop() {
   /****************** Receiving of Radio Transmission ***************************/
-  unsigned long received_signal;
-  radio.startListening();
-  if (radio.available()) {                // Variable for the received timestamp
-    while (radio.available()) {           // While there is data ready
-      radio.read(&received_signal, sizeof(unsigned long));  // Get the payload
-    }
+  byte received_signal = 0;
 
-    radio.stopListening();                // First, stop listening so we can talk
-    radio.write(&received_signal, sizeof(unsigned long));   // Send the final one back.
-
-    // Now, resume listening so we catch the next packets.
-    Serial.println(F("Got response"));
-    Serial.println(received_signal);
-  }
+  radio.startListening();  // Start the radio listening for data
+//  if (radio.available()) {                // Variable for the received timestamp
+    while (!radio.available()) {
+      delay(1);
+   }
+   radio.read(&received_signal, sizeof(byte));  // Get the payload
+   Serial.println((received_signal));
+//  }
+//  radio.stopListening();  // Start the radio listening for data
 
   leftSpeed = 180;
   rightSpeed = 0;
+  myservoLeft.attach(9);
+  myservoRight.attach(8);
 
   if (received_signal == 0) {
-    Serial.println("STOP");
     myservoLeft.detach();
     myservoRight.detach();
     digitalWrite(12, HIGH);
     digitalWrite(10, LOW);
-  } else {
-    myservoLeft.attach(9);
-    myservoRight.attach(8);
-    if (received_signal == 1) {
+    go = 0;
+    Serial.println("STOP");
+  } else if (received_signal == 1) {
       Serial.println("Forward");
       leftSpeed = 180;
       rightSpeed = 0;
       digitalWrite(12, LOW);
       digitalWrite(10, HIGH);
       go = 1;
-    } else if (received_signal == 2) {
+   } else if (received_signal == 2) {
       Serial.println("Backward");
       leftSpeed = 0;
       rightSpeed = 180;
@@ -102,14 +99,8 @@ void loop() {
     } else {
       Serial.println("No Signal Received");
     }
-  }
-
-  if (go == 1) {
     myservoLeft.write(leftSpeed);
     myservoRight.write(rightSpeed);
-  }
-
-  Serial.println(go);
 
   Serial.println("------------New round----------");
 
